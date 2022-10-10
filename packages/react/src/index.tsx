23bc53b40e15294421635a59009f08ece364a696
amanda-santos <amandasf0200@gmail.com>
2022-10-10 20:01:37 -0300

    chore: 🔧 setup react

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,6 @@
+import { colors } from '@ignite-ui/tokens'
+import { ReactElement } from 'react'
+
+export const App = (): ReactElement => {
+  return <h1 style={{ color: colors.ignite900 }}>Hello world</h1>
+}
